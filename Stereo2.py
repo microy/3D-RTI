a99e8fb072744f3d17e930ba77a03e223d4a0d96
Michaël Roy <microygh@gmail.com>
2017-10-20 18:03:01 +0200

    Startup numpyfy the normal map calculation.

--- Stereo2.py
+++ Stereo2.py
@@ -13,28 +13,49 @@ def GetNormalMap( lights, images ) :
 	normals = np.zeros( ( nrows, ncols, 3 ) )
 	normals[ :, :, 2 ] = 1
 	albedo = np.zeros( ( nrows, ncols ) )
+	lights_t = lights.transpose()
+	A = lights_t.dot( lights )
+	A_inv = np.linalg.inv( A )
+
+	# V1
 	for i in range( nrows ) : # y
 		for j in range( ncols ) : # x
 			I = images[:, i, j]
-			NP, R = PixelNormal( I, lights )
-			normals[ i, j ] =  NP
+			if i == 1000 and j == 1000: print( I.shape )
+			I = I.transpose()
+			if i == 1000 and j == 1000: print( I.shape )
+			b = lights_t.dot( I )
+			g = A_inv.dot( b )
+			R = np.linalg.norm( g )
+			N = g / R
+			if np.linalg.norm( I ) < 1.0E-06 :
+				N = 0
+				R = 0
+			normals[ i, j ] =  N
 			albedo[ i, j ]= R
+
+	# V2
+	# for i in range( nrows ) : # y
+	# 	I = images[:, i, :]
+	# 	I = I.transpose()
+	# 	b = lights_t.dot( I )
+	# 	print( I.shape )
+	# 	g = np.linalg.inv( A ).dot( b )
+	# 	R = np.linalg.norm( g )
+	# 	N = g / R
+	# 	if np.linalg.norm( I ) < 1.0E-06 :
+	# 		N = 0
+	# 		R = 0
+	#
+	# 	normals[ i, : ] =  N
+	# 	albedo[ i, : ]= R
+
+
+
 	maxval = albedo.max()
 	if maxval > 0 : albedo /= maxval
 	return normals, albedo
 
-def PixelNormal( I, L ) :
-	I = I.transpose()
-	LT = L.transpose()
-	A = LT.dot( L )
-	b = LT.dot( I )
-	g = np.linalg.inv( A ).dot( b )
-	R = np.linalg.norm( g )
-	N = g / R
-	if np.linalg.norm( I ) < 1.0E-06 :
-		N = 0
-		R = 0
-	return N, R
 
 # Compute the depth map
 def GetDepthMap( normals ) :
