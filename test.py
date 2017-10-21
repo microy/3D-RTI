292f128cffbe5acbac311059f13fb5915d0ee5ed
Michaël Roy <microygh@gmail.com>
2017-10-21 12:09:31 +0200

    Get the albedo and remove the gradients from the first stereo implementation.

--- test.py
+++ test.py
@@ -21,23 +21,25 @@ if __name__ == '__main__' :
 	# Compute normal map
 	print( 'Computing normal map...' )
 	# Stereo 1
-#	normals, pgrads, qgrads = Stereo1.GetNormalMap( lights, images )
+	normals, albedo = Stereo1.GetNormalMap( lights, images )
 	# Stereo 2
-	normals, albedo = Stereo2.GetNormalMap( lights, images )
+#	normals, albedo = Stereo2.GetNormalMap( lights, images )
 
 	# Convert the normal map into an image
 	normalmap_image = cv2.cvtColor( normals.astype( np.float32 ), cv2.COLOR_BGR2RGB )
+	# Dipslay the normal map and the albedo
 	cv2.imshow( 'Normal Map',  normalmap_image )
 	cv2.imshow( 'Albedo',  albedo )
 	cv2.waitKey()
-	# Write the normal map
+	cv2.destroyAllWindows()
+	# Save the normal map and the albedo
 	cv2.imwrite( 'normalmap.png',  normalmap_image  * 255.99 )
 	cv2.imwrite( 'albedo.png',  albedo  * 255.99 )
 
 	# Compute the depth map
-#	print( 'Computing depth map...' )
-#	z = Stereo.GetDepthMap( pgrads, qgrads )
+	print( 'Computing depth map...' )
+	z = Stereo1.GetDepthMap( normals )
 
 	# Triangulate the depth map, and export the mesh to a PLY file
-#	print( 'Exporting mesh...' )
-#	Mesh.ExportMesh( z, 'mesh.ply' )
+	print( 'Exporting mesh...' )
+	Mesh.ExportMesh( z, 'mesh.ply' )
