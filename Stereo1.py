fea125adb82cbc0abe09b7d0994e5ff4b4b0e827
Michaël Roy <microygh@gmail.com>
2017-10-22 20:36:26 +0200

    Fix division by zero. Typo.

--- Stereo1.py
+++ Stereo1.py
@@ -5,19 +5,18 @@
 #
 
 # External dependencies
-import math
 import cv2
 import numpy as np
 
 # Estimate the normals
 def GetNormalMap( lights, images ) :
 	# Get the image size
-	height, width = images[0].shape[:2]
+	height, width = images[0].shape[ :2 ]
 	# Compute the pseudo-inverse of the light position matrix using SVD
 	_, lights_inv = cv2.invert( lights, flags = cv2.DECOMP_SVD )
 	# Initialize the normals
-	normals = np.zeros( (height, width, 3) )
-	albedo = np.zeros( (height, width) )
+	normals = np.zeros( ( height, width, 3 ) )
+	albedo = np.zeros( ( height, width ) )
 	# Compute the normal for each pixel
 	for y in range( height ) :
 		I = images[:, y, :]
@@ -25,7 +24,7 @@ def GetNormalMap( lights, images ) :
 		p = np.sqrt( ( n ** 2 ).sum( axis = 1 ) )
 		condition = p > 0
 		n[condition] /= p[condition].reshape( (-1, 1) )
-		n[~condition] = [0, 0, 1]
+		n[~condition] = [ 0, 0, 1 ]
 		normals[y, :] = n
 		albedo[y, :] = p
 	# Normalize the albedo
@@ -36,18 +35,18 @@ def GetNormalMap( lights, images ) :
 # Compute the depth map
 def GetDepthMap( normals ) :
 	# Get the image size
-	height, width = normals.shape[:2]
+	height, width = normals.shape[ :2 ]
 	# Initialize the pgrads, qgrads matrices
 	pgrads = np.zeros( (height, width) )
 	qgrads = np.zeros( (height, width) )
 	# Compute the gradients
 	for y in range( height ) :
-		n = normals[y, :]
-		pgrads[y, :] = n[:, 0] / n[:, 2]
-		qgrads[y, :] = n[:, 1] / n[:, 2]
+		n = normals[ y, : ]
+		pgrads[ y, : ] = n[ :, 0 ] / n[ :, 2 ]
+		qgrads[ y, : ] = n[ :, 1 ] / n[ :, 2 ]
 	# Compute the Fourier Transformation of the gradients
-	P = cv2.dft( pgrads, flags = cv2.DFT_COMPLEX_OUTPUT )
-	Q = cv2.dft( qgrads, flags = cv2.DFT_COMPLEX_OUTPUT )
+	p = cv2.dft( pgrads, flags = cv2.DFT_COMPLEX_OUTPUT )
+	q = cv2.dft( qgrads, flags = cv2.DFT_COMPLEX_OUTPUT )
 	# Initilize the depth
 	z = np.zeros( (height, width, 2) )
 	# v1
@@ -61,13 +60,19 @@ def GetDepthMap( normals ) :
 	# 		Z[y, x, 0] = ( u*P[y, x, 1] + v*Q[y, x, 1]) / d
 	# 		Z[y, x, 1] = (-u*P[y, x, 0] - v*Q[y, x, 0]) / d
 	# v2
-	u, v = np.meshgrid( np.linspace( 0, 2*np.pi, height, endpoint = False ), np.linspace( 0, 2*np.pi, width, endpoint = False ) )
+	u = np.linspace( 0, 2 * np.pi, height, endpoint = False )
+	v = np.linspace( 0, 2 * np.pi, width, endpoint = False )
+	u, v = np.meshgrid( u, v )
 	u = np.sin( u ).transpose()
 	v = np.sin( v ).transpose()
-	uv = np.sin( u ) ** 2 + np.sin( v ) ** 2
+	uv = u ** 2 + v ** 2
 	d = 2 * uv + uv ** 2
-	z[:, :, 0] = (  u*P[:, :, 1] + v*Q[:, :, 1] ) / d
-	z[:, :, 1] = ( -u*P[:, :, 0] - v*Q[:, :, 0] ) / d
-	z[0, 0] = 0.0
+	# Fix division by zero
+	d[ 0, 0 ] = 1
+	# Compute the depth map
+	z[ :, :, 0 ] = (  u * p[ :, :, 1 ] + v * q[ :, :, 1 ] ) / d
+	z[ :, :, 1 ] = ( -u * p[ :, :, 0 ] - v * q[ :, :, 0 ] ) / d
+	# Fix
+	z[ 0, 0 ] = 0
 	z = cv2.dft( z, flags = cv2.DFT_INVERSE | cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT )
 	return z
