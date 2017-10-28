c691f38e90f419728801951b6339210aa1130108
Michaël Roy <microygh@gmail.com>
2017-10-28 18:48:56 +0200

    Use numpy for the FFT.

--- xRTI/Depth.py
+++ xRTI/Depth.py
@@ -5,7 +5,7 @@
 #
 
 # External dependencies
-import cv2
+#import cv2
 import numpy as np
 
 #
@@ -21,10 +21,13 @@ def GetDepth( normals ) :
 	pgrads = normals[ :, :, 0 ] / normals[ :, :, 2 ]
 	qgrads = normals[ :, :, 1 ] / normals[ :, :, 2 ]
 	# Compute the Fourier Transformation of the gradients
-	p = cv2.dft( pgrads, flags = cv2.DFT_COMPLEX_OUTPUT )
-	q = cv2.dft( qgrads, flags = cv2.DFT_COMPLEX_OUTPUT )
+#	p = cv2.dft( pgrads, flags = cv2.DFT_COMPLEX_OUTPUT )
+#	q = cv2.dft( qgrads, flags = cv2.DFT_COMPLEX_OUTPUT )
+	p = np.fft.fft2( pgrads )
+	q = np.fft.fft2( qgrads )
 	# Initialize the depth
-	z = np.zeros( ( height, width, 2 ) )
+#	z = np.zeros( ( height, width, 2 ) )
+	z = np.zeros( ( height, width ), dtype = np.complex128 )
 	#
 	u = np.linspace( 0, 2 * np.pi, height, endpoint = False )
 	v = np.linspace( 0, 2 * np.pi, width,  endpoint = False )
@@ -34,12 +37,16 @@ def GetDepth( normals ) :
 	# Fix division by zero
 	d[ 0, 0 ] = 1
 	# Compute the depth map
-	z[ :, :, 0 ] = (  u * p[ :, :, 1 ] + v * q[ :, :, 1 ] ) / d
-	z[ :, :, 1 ] = ( -u * p[ :, :, 0 ] - v * q[ :, :, 0 ] ) / d
+#	z[ :, :, 0 ] = (  u * p[ :, :, 1 ] + v * q[ :, :, 1 ] ) / d
+#	z[ :, :, 1 ] = ( -u * p[ :, :, 0 ] - v * q[ :, :, 0 ] ) / d
+	z[ :, : ].real = (  u * p.imag + v * q.imag ) / d
+	z[ :, : ].imag = ( -u * p.real - v * q.real ) / d
 	# Fix
-	z[ 0, 0 ] = 0
+	z[ 0, : ] = 0 + 0j
+	z[ :, 0 ] = 0 + 0j
 	# Inverse Fourier transformation of the depth map
-	z = cv2.dft( z, flags = cv2.DFT_INVERSE | cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT )
+#	z = cv2.dft( z, flags = cv2.DFT_INVERSE | cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT )
+	z = np.fft.ifft2( z ).real
 	# Return the depth map
 	return z
 
